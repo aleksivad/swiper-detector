df3370dc88ab675c2a1d3d8be8604e470c3b7d16
Aleksandra Adamovic <a.adamovic@dvcsolutions.com>
2021-05-04 22:19:03 +0200

    Modified library

--- swipe-detector.js
+++ swipe-detector.js
@@ -1,24 +1,36 @@
-function SwipeLeftRightDetector(el, swipeLeft, swipeRight) {
+/**
+ *
+ * @param {*} el HTMLElement
+ * @param {*} args {onSwipeLeft: ?Function, onSwipeRight: ?Function, threshold: ?Number, onMove: ?Function}
+ */
+ function SwipeRightLeftDetector(el, args) {
 
-    var start, end;
-
+    var start, end = null;
+    var threshold = args.threshold || 100;
     el.addEventListener('touchstart', handleStart);
     el.addEventListener("touchmove", handleMove);
     el.addEventListener('touchend', handleEnd);
 
     function handleStart(evt) {
         start = evt.touches[0].clientX;
+        end = null;
     };
 
     function handleMove(evt) {
         end = evt.touches[0].clientX;
+        typeof args.onMove == 'function' && args.onMove(start - end);
     };
 
     function handleEnd(evt) {
+        if(end === null)
+            return;
+        if (Math.abs(start - end) < threshold || !end) {
+            return;
+        }
         if(start > end) {
-            swipeLeft();
+            (typeof args.onSwipeLeft == 'function') && args.onSwipeLeft();
         } else {
-            swipeRight();
+            (typeof args.onSwipeRight == 'function') && args.onSwipeRight();
         };
     };
 
